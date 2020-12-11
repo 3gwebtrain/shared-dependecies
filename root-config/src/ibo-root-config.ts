5152cbb17bde0a08b78db09a39c38e9c890e3462
Mohamed Arif <3gwebtrain@gmail.com>
2020-12-11 14:46:00 +0530

    trial pushed

---
+++ root-config/src/ibo-root-config.ts
@@ -0,0 +1,20 @@
+import { registerApplication, start } from "single-spa";
+
+
+registerApplication(
+  "login",
+  () => System.import("@ibo/login"),
+  (location) => !location.pathname.startsWith("/main")
+);
+
+
+/* 
+  Enables production mode in angular as it can only be set once and be in one mode for the whole system.
+  Angular apps no longer need to call enableProdMode() as It would produce an error.
+*/
+System.import("@angular/core").then(a => {
+  a.enableProdMode();
+  start({
+    urlRerouteOnly: true,
+  });
+});
---
+++ root-config/webpack.config.js
@@ -0,0 +1,40 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "ibo";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+  });
+
+  const merge = webpackMerge({
+    customizeArray: webpackMerge.unique(
+      "plugins",
+      ["HtmlWebpackPlugin"],
+      (plugin) => plugin.constructor && plugin.constructor.name
+    ),
+  });
+
+  return merge(
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: "src/index.ejs",
+          templateParameters: {
+            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
+            orgName,
+          },
+        }),
+      ],
+    },
+    defaultConfig,
+    {
+      devtool: 'cheap-module-source-map'
+    }
+  );
+};
